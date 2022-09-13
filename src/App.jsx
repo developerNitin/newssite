66abd650c90086aebd19ca5a29ed5ed52358310f
nitin <nitin.birdi@icloud.com>
2022-09-14 01:59:37 +0530

    initialized tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="min-h-screen min-w-screen bg-zinc-900 text-white">
+      <p className="font-extrabold text-2xl">Hello, World</p>
+    </main>
+  );
+}
